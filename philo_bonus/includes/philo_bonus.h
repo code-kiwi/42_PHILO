9e6552745dfb5019854df1dc626b450eb7d0cc4c
mhotting <madric.hottinger.101@gmail.com>
2024-03-11 16:16:02 +0100

    Project basic setup

---
+++ philo_bonus/includes/philo_bonus.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 16:08:50 by mhotting          #+#    #+#             */
+/*   Updated: 2024/03/11 16:12:49 by mhotting         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <string.h>
+# include <pthread.h>
+
+#endif
