9b2d923a86fad67e38de7156cc8bc9614c6b9731
mhotting <madric.hottinger.101@gmail.com>
2024-05-11 14:16:38 +0200

    Some dummy stuff

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: mhotting <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 16:08:13 by mhotting          #+#    #+#             */
-/*   Updated: 2024/03/11 16:08:42 by mhotting         ###   ########.fr       */
+/*   Updated: 2024/05/11 11:04:09 by mhotting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(void)
 {
-	printf("Hello from Philo\n");
+	struct timeval	curr_time;
+	gettimeofday(&curr_time, NULL);
+	printf("Seconds: %ld\nMicroseconds: %ld\n", curr_time.tv_sec, curr_time.tv_usec);
 	return (0);
 }
